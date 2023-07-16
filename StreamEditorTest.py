ed34f96fb9f36acf57d582bcfd5603e8b3262954
Viraj <virajmohit7@gmail.com>
2023-07-15 17:53:58 -0700

    combining

--- StreamEditorTest.py
+++ StreamEditorTest.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_ace import st_ace
+from langchain.chat_models import ChatOpenAI
+from langchain import LLMChain
 import time
 import os
 import psutil
@@ -24,13 +26,58 @@ def exec_code(code):
 
     return result, elapsed_time, memory_used
 
-def PromptingAndSubmit():
-    # returns user's input
-    input = null
+os.environ["OPENAI_API_KEY"] = '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'
+chat = ChatOpenAI(model="gpt-4", temperature=0) 
 
-def ProblemGenerator(user_input):
-    # returns GPT's output
-    input = null
+
+def prompting_and_submit_streamlit():
+    # Create a text input widget
+    user_input = st.text_input("problem area: ")
+
+    # Create a submit button
+    if st.button("Submit"):
+        # If the Submit button is pushed
+        return user_input
+    return None
+
+def complexity_level():
+    # Create a selectbox with options 1 through 5
+    selection = st.selectbox('Choose a number:', options=[1, 2, 3, 4, 5])
+    # Return the selection
+    return selection
+
+def ProblemGenerator(user_input, level):
+    smg = '''You are an AI code examiner. You have to create an examination task for Python developers.
+    Inputs are the problem area, and level of complexity(from 1 to 5), create a task based on that. Here is an example of a task:
+    ------------------
+    'You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    Example 1:
+    Input: l1 = [2,4,3], l2 = [5,6,4]
+    Output: [7,0,8]
+    Explanation: 342 + 465 = 807.
+    Example 2:
+    Input: l1 = [0], l2 = [0]
+    Output: [0]
+    Example 3:
+    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    Output: [8,9,9,9,0,0,0,1]
+    Constraints:
+    The number of nodes in each linked list is in the range [1, 100].
+    0 <= Node.val <= 9
+    It is guaranteed that the list represents a number that does not have leading zeros.'
+    ------------------
+    Your output should look exactly like that. Even if the input is not obviously a problem area, make a task.
+    Never output anything else, only the task.
+    '''
+    messages = [
+        SystemMessage(content=smg),
+        HumanMessage(content=f'problem area: + {user_input} +\n\n + level: + {level}')
+    ]
+    
+    result = chat(messages)
+    #print(result.content)
+    return result.content
 
 def CodeEditorAndExecute():
     # returns user code and execution result
@@ -57,3 +104,9 @@ def CodeEditorAndExecute():
 def ScoreCalculator(user_code, execution_result, time_to_execute, mem_alloc):
     # returns score and feedback
     input = null
+
+prompt = prompting_and_submit_streamlit()
+level = complexity_level()
+problem = ProblemGenerator(prompt, level)
+user_code, execution_result, time_to_execute, mem_alloc = CodeEditorAndExecute()
+# score, feedback = ScoreCalculator(user_code, execution_result, time_to_execute, mem_alloc)
